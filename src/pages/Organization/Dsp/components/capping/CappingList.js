2806359e0e0ebab92525739546f48d6e0953c729
ThinhPhan <thinhpd6393@gmail.com>
2022-01-12 10:49:12 +0700

    feat: 🎸 Add GUI list capping

--- src/pages/Organization/Dsp/components/capping/CappingList.js
+++ src/pages/Organization/Dsp/components/capping/CappingList.js
@@ -1,7 +1,78 @@
+import {List} from 'components/list';
 import React from 'react';
+import {useTranslation} from 'react-i18next';
+import {Badge} from 'reactstrap';
+import {CAPPINGS} from './mockData';
 
 const CappingList = () => {
-  return <></>;
+  const {t} = useTranslation();
+  const cappings = React.useMemo(
+    () =>
+      CAPPINGS?.map(item => ({
+        ...item,
+        id: item?.uuid,
+        smooth: item?.smooth ? 'true' : 'false',
+        climit: item?.climit?.toString() || '',
+        time_frame: item?.time_frame?.toString() || ''
+      })),
+    []
+  );
+
+  //---> Define columns
+  const columns = React.useMemo(() => {
+    return [
+      {
+        header: 'Capping type',
+        accessor: 'ctype'
+      },
+      {
+        header: 'Time frame',
+        accessor: 'time_frame',
+        cell: row => (
+          <Badge color="primary" pill>
+            {row?.value}
+          </Badge>
+        )
+      },
+      {
+        header: 'Limit',
+        accessor: 'climit',
+        cell: row => (
+          <Badge color="primary" pill>
+            {row?.value}
+          </Badge>
+        )
+      },
+      {
+        header: 'Smooth',
+        accessor: 'smooth',
+        cell: row => {
+          return (
+            <Badge color={row?.value === 'true' ? 'success' : 'warning'}>
+              {row?.value}
+            </Badge>
+          );
+        }
+      }
+    ];
+  }, []);
+
+  function onClickMenu() {}
+
+  function onClickItem() {}
+
+  return (
+    <div>
+      <List
+        data={cappings || []}
+        columns={columns}
+        showAction
+        actions={[t('edit'), t('delete')]}
+        handleAction={onClickMenu}
+        handleClickItem={onClickItem}
+      />
+    </div>
+  );
 };
 
 export default CappingList;
