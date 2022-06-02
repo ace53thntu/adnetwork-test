32ec3d2fbb14b48639f4ccf7ee11b829bf9c2e2c
mynguyenthe <my.nguyen.the@aiactiv.io>
2022-06-02 13:08:56 +0700

    chore(config): 🎸 Change API config to aicactus

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.ADN_API_ENDPOINTS = {
-  AUTH_GATEWAY: 'https://staging-adnetwork-api.aiactiv.io',
-  API_GATEWAY: 'https://staging-adnetwork-api.aiactiv.io',
+  AUTH_GATEWAY: 'https://staging-adnetwork-api.aicactus.io',
+  API_GATEWAY: 'https://staging-adnetwork-api.aicactus.io',
   UPLOADER_GATEWAY: 'https://staging-adnetwork-uploader.aicactus.io'
 };
 window.ADN_META_DATA = {
