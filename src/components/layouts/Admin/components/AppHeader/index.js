274db7257e1b98fe1cc0e3dab626d4fc31192fd0
Hieu Luyen <hieu.luyen@aiactiv.io>
2022-06-03 13:32:42 +0700

    feat: 💄 update icon for toggle side bar

--- src/components/layouts/Admin/components/AppHeader/index.js
+++ src/components/layouts/Admin/components/AppHeader/index.js
@@ -1,23 +1,30 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import cx from 'classnames';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import CSSTransition from 'react-transition-group/CSSTransition';
-
 import HeaderLogo from '../AppLogo';
 
 // import SearchBox from './components/SearchBox';
 import UserBox from './components/UserBox';
+import { ReactComponent as IconSideBar } from '../../../../../assets/utils/images/sidebar/icon-side-bar.svg';
 
 import HeaderDots from './components/HeaderDots';
+import { setEnableClosedSidebar } from 'store/reducers/ThemeOptions';
 
 class Header extends React.Component {
+  toggleSideBar = () => {
+    const { enableClosedSidebar, setToggleSidebar } = this.props;
+    setToggleSidebar(!enableClosedSidebar);
+  }
+
   render() {
     let {
       headerBackgroundColor,
       enableMobileMenuSmall,
-      enableHeaderShadow
+      enableHeaderShadow,
+      enableClosedSidebar
     } = this.props;
     return (
       <Fragment>
@@ -28,7 +35,13 @@ class Header extends React.Component {
             })}
           >
             <HeaderLogo />
-
+            <div className='sidebar-toggle'>
+              {React.createElement(IconSideBar, {
+                className: 'trigger',
+                onClick: this.toggleSideBar,
+                title: enableClosedSidebar ? 'Collapsed' : 'Expanded'
+              })}
+            </div>
             <div
               className={cx('app-header__content', {
                 'header-mobile-open': enableMobileMenuSmall
@@ -53,9 +66,12 @@ const mapStateToProps = state => ({
   enableHeaderShadow: state.ThemeOptions.enableHeaderShadow,
   closedSmallerSidebar: state.ThemeOptions.closedSmallerSidebar,
   headerBackgroundColor: state.ThemeOptions.headerBackgroundColor,
-  enableMobileMenuSmall: state.ThemeOptions.enableMobileMenuSmall
+  enableMobileMenuSmall: state.ThemeOptions.enableMobileMenuSmall,
+  enableClosedSidebar: state.ThemeOptions.enableClosedSidebar
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  setToggleSidebar: status => dispatch(setEnableClosedSidebar(status))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
