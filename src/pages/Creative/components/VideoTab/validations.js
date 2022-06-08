8e3c55c6bf0d2eb03ac89450867b14e15a6ce00d
Em Dinh <em.dinh@aicactus.io>
2022-06-08 15:19:21 +0700

    fix: 🐛 validate third party tag type

--- src/pages/Creative/components/VideoTab/validations.js
+++ src/pages/Creative/components/VideoTab/validations.js
@@ -46,6 +46,17 @@ export function createVideoFormResolver() {
           return true;
         }
       ),
+      third_party_tag_type: Yup.object()
+        .nullable()
+        .when('type', {
+          is: type => type.value === 'third_party',
+          then: Yup.object()
+            .shape({
+              label: Yup.string().required('Required.'),
+              value: Yup.string().required('Required.')
+            })
+            .required('Required.')
+        }),
       third_party_tag: Yup.string().when('type', {
         is: type => type.value === 'third_party',
         then: Yup.string().required('Required.')
