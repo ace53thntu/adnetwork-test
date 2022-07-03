cfab1bbb09e9126f6dad5e5d7866a4ca504a3d6f
Em Dinh <em.dinh@aicactus.io>
2022-07-03 10:29:17 +0700

    feat: 🎸 save as strategy

--- src/pages/Campaign/strategy/form-action/FormAction.js
+++ src/pages/Campaign/strategy/form-action/FormAction.js
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
 import {ButtonLoading} from 'components/common';
 import {useCampaignManager} from 'pages/Campaign/hooks';
 import {useFormContext} from 'react-hook-form';
+import {useSaveAsStrategy} from 'pages/Campaign/hooks/useSaveAsStrategy';
 
 const propTypes = {
   isView: PropTypes.bool,
@@ -21,19 +22,35 @@ const FormAction = ({
   isView = false,
   isSummary = false,
   isCreate = false,
-  currentStrategy = {}
+  currentStrategy = {},
+  originalStrategy
 }) => {
   const {t} = useTranslation();
   const {gotoCampaignManagement} = useCampaignManager();
   const {
     formState: {isSubmitting, isDirty}
   } = useFormContext();
+  const {onHandleSaveAs, isSubmitting: isSavingAs} = useSaveAsStrategy(
+    currentStrategy,
+    originalStrategy
+  );
 
   return (
     <>
       {!isView && (
         <div className="d-block text-right mr-15">
           <hr />
+
+          <Button
+            onClick={onHandleSaveAs}
+            className="mr-2"
+            color="info"
+            type="button"
+            disabled={isSavingAs}
+            loading={isSavingAs}
+          >
+            Save as
+          </Button>
 
           <Button
             onClick={gotoCampaignManagement}
